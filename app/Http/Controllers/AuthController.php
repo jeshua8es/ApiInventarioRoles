f29d760e5a0b54550550488e736bb7c95ac2c09f
Andres Caicedo <jeshua8es@gmail.com>
2025-05-14 15:03:34 -0500

    Corregida asignación de roles en AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,9 +13,10 @@ class AuthController extends Controller
 {
     public function register(Request $request){
         $validator = Validator::make($request->all(),[
-            'name' => 'required|string|min:10',
+            'name' => 'required|string|min:3',
             'email' => 'required|string|email|min:10|max:50|unique:users',
-            'password' => 'required|string|min:10|confirmed',
+            'password' => 'required|string|min:5|confirmed',
+            'role' => 'required|string|in:admin,user'
         ]);
         if($validator->fails()){
             return response()->json(['error' => $validator->errors()],422);
@@ -25,14 +26,14 @@ class AuthController extends Controller
             'role'=>$request->get('role'),
             'email'=>$request->get('email'),
             'password'=>bcrypt($request->get('password')),
-            'role' => 'user',
+            'role' => $request->get('role'),
         ]);
         return response()->json(['message'=>'User created succesfully'],201);
     }
     public function login(Request $request){
         $validator = Validator::make($request->all(),[
-            'email' => 'required|string|email|min:10|max:50',
-            'password' => 'required|string|min:10',
+            'email' => 'required|string|email|min:5|max:50',
+            'password' => 'required|string|min:5',
         ]);
         if($validator->fails()){
             return response()->json(['error' => $validator->errors()],422);
